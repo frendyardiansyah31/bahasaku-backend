8a881ad11c9a94cad44a620ef7cac2cfc405a645
frendyardiansyah31 <frendyardiansyah31@gmail.com>
2026-05-13 23:43:22 +0700

    fix error build

--- bahasaku/settings.py
+++ bahasaku/settings.py
@@ -9,6 +9,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-change-in-production')
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
